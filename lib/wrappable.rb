83b0311b277e69071bed739ac5569da0f1c9c48b
Senthil A <senthil196@gmail.com>
2012-04-04 14:46:19 -0700

    basic dsl, added endpoint/node

--- lib/wrappable.rb
+++ lib/wrappable.rb
@@ -1,4 +1,34 @@
 require_relative 'wrappable/version.rb'
 
 module Wrappable
+  def app
+    @app ||= App.new
+  end
+
+  def method_missing(name, *args, &blk)
+    app.send(name, *args, &blk)
+  end
+
+  def self.const_missing(name)
+    App.const_get(name)
+  end
+
+  class App
+    def initialize
+      @nodes = []
+    end
+
+    def endpoint(url=nil)
+      @endpoint = url if url
+      @endpoint
+    end
+
+    def node(name)
+      current_node = Node.new
+      self.class.const_set(name.to_s.capitalize, current_node)
+    end
+  end
+
+  class Node
+  end
 end
--- spec/wrappable_spec.rb
+++ spec/wrappable_spec.rb
@@ -1,6 +1,39 @@
 require 'spec_helper'
 require 'wrappable'
 
-describe Wrappable do
-  it
+module Wrappable; describe Wrappable do
+  subject do
+    Module.new do
+      extend Wrappable
+
+      node :calendars
+    end
+  end
+
+  it 'delegates methods to App' do
+    Wrappable::App.any_instance.should_receive(:node)
+    subject
+  end
+
+  it 'delegates constants to App'
+end
+
+describe App do
+  subject do
+    mod = Module.new do
+      extend Wrappable
+
+      endpoint 'google'
+      node :calendars
+    end
+  end
+
+  it 'allows endpoint getters/setters' do
+    subject.endpoint.should == 'google'
+  end
+
+  it 'sets constant to newly created node' do
+    Calendars.should be_a(Node)
+  end
+end
 end
