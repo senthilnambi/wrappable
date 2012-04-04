4657cc7957ac45b5a7566bff85ca569f28cacfc7
Senthil A <senthil196@gmail.com>
2012-04-04 15:37:39 -0700

    add block support to node dsl; sets parent in nested nodes

--- lib/wrappable.rb
+++ lib/wrappable.rb
@@ -22,18 +22,41 @@ module Wrappable
     end
 
     def node(name)
-      current_node = Node.new(name, nil)
+      current_node = Node.new(name, @current_node)
       nodes << current_node
+
+      current_node_scope(current_node) do
+        yield if block_given?
+      end
     end
 
+    private
+
     def method_missing(name, *args, &blk)
-      existing_node = nodes.find {|node| node.name == name }
+      existing_node = find_node(name)
       existing_node ? existing_node : super
+    end
+
+    def current_node_scope(node)
+      scope(:current_node, node) do
+        yield
+      end
+    end
+
+    def scope(name, value=nil)
+      instance_variable_set("@#{name}", value)
+      yield
+    ensure
+      instance_variable_set("@#{name}", nil)
+    end
+
+    def find_node(name)
+      nodes.find {|node| node.name == name }
     end
   end
 
   class Node
-    attr_reader :name
+    attr_reader :name, :parent
 
     def initialize(name, parent)
       @name = name
--- spec/wrappable_spec.rb
+++ spec/wrappable_spec.rb
@@ -18,10 +18,11 @@ end
 
 describe App do
   subject do
-    mod = Module.new do
+    Module.new do
       extend Wrappable
 
       endpoint 'google'
+
       node :calendars
     end
   end
@@ -30,8 +31,45 @@ describe App do
     subject.endpoint.should == 'google'
   end
 
+  it 'allows defining of RESTful nodes' do
+    subject.nodes.count.should == 1
+  end
+
   it 'defines getter for node' do
     subject.calendars.should be_a(Node)
+  end
+
+  context 'when nested' do
+    subject do
+      Module.new do
+        extend Wrappable
+
+        node :calendars do
+          node :acl
+        end
+        node :events
+      end
+    end
+
+    it 'allows nesting of nodes' do
+      subject.nodes.count.should == 3
+    end
+
+    it 'remembers parent node' do
+      acl = find_node(subject.nodes, :acl)
+      cal = find_node(subject.nodes, :calendars)
+
+      acl.parent.should == cal
+    end
+
+    it 'does not affect non-nested nodes' do
+      events = find_node(subject.nodes, :events)
+      events.parent.should == nil
+    end
+  end
+
+  def find_node(nodes, name)
+    nodes.find { |node| node.name == name }
   end
 end
 end
