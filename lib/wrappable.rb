fb7218bd879c74f0b956f4c9454defe8095ece08
Senthil A <senthil196@gmail.com>
2012-04-04 21:12:35 -0700

    moved App & specs to own files

--- lib/wrappable.rb
+++ lib/wrappable.rb
@@ -1,6 +1,7 @@
 require_relative 'wrappable/version'
 require_relative 'wrappable/finder'
 require_relative 'wrappable/node'
+require_relative 'wrappable/app'
 
 module Wrappable
   def app
@@ -9,49 +10,5 @@ module Wrappable
 
   def method_missing(name, *args, &blk)
     app.send(name, *args, &blk)
-  end
-
-  class App
-    include Finder
-
-    attr_reader :nodes
-
-    def initialize
-      @nodes = []
-    end
-
-    def endpoint(url=nil)
-      @endpoint = url if url
-      @endpoint
-    end
-
-    def node(name)
-      current_node = Node.new(name, @current_node)
-      nodes << current_node
-
-      current_node_scope(current_node) do
-        yield if block_given?
-      end
-    end
-
-    private
-
-    def current_node_scope(node)
-      scope(:current_node, node) do
-        yield
-      end
-    end
-
-    def scope(name, value=nil)
-      instance_variable_set("@#{name}", value)
-      yield
-    ensure
-      instance_variable_set("@#{name}", nil)
-    end
-
-    def method_missing(name, *args, &blk)
-      existing_node = find_in_array(nodes, name)
-      existing_node ? existing_node : super
-    end
   end
 end
---
+++ spec/app_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+module Wrappable; describe App do
+  include Wrappable::Finder
+
+  subject do
+    Module.new do
+      extend Wrappable
+
+      endpoint 'google'
+
+      node :calendars
+    end
+  end
+
+  it 'allows endpoint getters/setters' do
+    subject.endpoint.should == 'google'
+  end
+
+  it 'allows defining of RESTful nodes' do
+    subject.nodes.count.should == 1
+  end
+
+  it 'defines getter for node' do
+    subject.calendars.should be_a(Node)
+  end
+
+  context 'when nested' do
+    subject do
+      Module.new do
+        extend Wrappable
+
+        node :calendars do
+          node :acl
+        end
+        node :events
+      end
+    end
+
+    it 'allows nesting of nodes' do
+      subject.nodes.count.should == 3
+    end
+
+    it 'remembers parent node' do
+      acl = find_in_array(subject.nodes, :acl)
+      cal = find_in_array(subject.nodes, :calendars)
+
+      acl.parent.should == cal
+    end
+
+    it 'does not affect non-nested nodes' do
+      events = find_in_array(subject.nodes, :events)
+      events.parent.should == nil
+    end
+  end
+end
+end
